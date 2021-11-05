86cef072c288f58fca6ea22e2cfab39aaf7b937b
guen-ynk <64133590+guen-ynk@users.noreply.github.com>
2021-11-05 16:47:17 +0100

    Board.hpp, added has_lost function

--- board.cpp
+++ board.cpp
@@ -1,33 +1,216 @@
-#include "board.hpp"
-
-#include <vector>
+// sample.h
+#pragma once
+#include <vector> // #include directive
 #include <string>
- 
+#include "Eigen/Dense"
+#include <iostream>
 
 
 using namespace std;
 using namespace Eigen;
- 
-  
-int main(int argc, char const *argv[])
-{
-    Board board1 = Board();
-    
-    cout << "field =" << endl << board1.field << endl;
-    auto positions = board1.get_amazon_moves(Coordinates(3,9));
-    for (size_t i = 0; i < positions.size(); i++)
-    {
-        cout << positions[i].x << positions[i].y << endl;
-        board1.field(positions[i].x,positions[i].y) = 4;
+
+class Coordinates{
+    public:
+        short x,y;
+    Coordinates(short xx, short yy){
+        x = xx;
+        y = yy;
     }
-    cout << "field =" << endl << board1.field << endl;
+    void add(Coordinates sec){
+        x+=sec.x;
+        y+=sec.y;
+    }
+    void sub(Coordinates sec){
+        x-=sec.x;
+        y-=sec.y;
+    }
 
-     
+};
+class Board {
+    public:
+        bool whites_turn;
+        short board_size;
+        short amazons_count;
+        vector<Coordinates> operations {
+            Coordinates(1,0),Coordinates(-1,0),Coordinates(0,1),Coordinates(0,-1),
+            Coordinates(1,1),Coordinates(1,-1),Coordinates(-1,1),Coordinates(-1,-1)
+        };
 
-    // Liste:
-    // (0,4),(0,6),(3,0),(3,9)
+        Matrix<short,Dynamic,Dynamic> field;
+        
+        Board(){
+            whites_turn = true;
+            board_size = 10;
+            amazons_count = 4;
+            field =  Matrix <short , 10, 10>::Zero();
 
+            field(0,3) = 1;
+            field(0,6) = 1;
+            field(3,0) = 1;
+            field(3,9) = 1;
 
+            field(9,3) = 2;
+            field(9,6) = 2;
+            field(6,0) = 2;
+            field(6,9) = 2;
+            
+        }
 
-    return 0;
-}
+        /*
+            returns vector of coordinates ( amazons )
+        **/
+        vector<Coordinates> get_queen_positions(short color){
+            short counter = 0;
+            vector<Coordinates> positions;
+            for (size_t i = 0; i < board_size; i++)
+            {
+                for (size_t j = 0; j < board_size; j++)
+                {
+                    if(field(i,j)== color){
+                        positions.push_back(Coordinates(i,j));
+                        counter++;
+                        if (counter == amazons_count)return positions;
+                    }
+                    
+                }
+                
+            }
+            return positions;
+            
+        }
+         /*
+            returns vector of coordinates ( reachable for amazon at position c )
+        **/
+        vector<Coordinates> get_amazon_moves(Coordinates c){
+            short x = c.x;
+            short y = c.y;                        
+            vector<Coordinates> positions;
+            
+            // North
+            int i = x-1;
+            do{   
+                if(field(i,y)==0){
+                    positions.push_back(Coordinates(i,y));
+                }else{
+                    break;
+                } 
+            }while(i--);
+
+            // South
+            i = x+1;
+            for (; i < board_size; i++)
+            {
+                if(field(i,y)==0){
+                    positions.push_back(Coordinates(i,y));
+                }else{
+                    break;
+                } 
+            }
+            
+            // West
+            i = y-1;
+            do{   
+                if(field(x,i)==0){
+                    positions.push_back(Coordinates(x,i));
+                }else{
+                    break;
+                } 
+            }while(i--);
+
+            // East
+            i = y+1;
+            for (; i < board_size; i++)
+            {
+                if(field(x,i)==0){
+                    positions.push_back(Coordinates(x,i));
+                }else{
+                    break;
+                } 
+            }
+
+            // SW
+            x = c.x;
+            y = c.y; 
+            i = 0;
+            for (; i < board_size; i++)
+            {
+                x -= 1;
+                y -= 1;
+                if( x>=0 && y>=0 && field(x,y)==0){
+                    positions.push_back(Coordinates(x,y));
+                }else{
+                    break;
+                }
+            }
+
+            // NE
+            x = c.x;
+            y = c.y; 
+            i = 0;
+            for (; i < board_size; i++)
+            {
+                x += 1;
+                y += 1;
+                if( x<board_size && y<board_size && field(x,y)==0){
+                    positions.push_back(Coordinates(x,y));
+                }else{
+                    break;
+                }
+            }
+
+            // SE
+            x = c.x;
+            y = c.y; 
+            i = 0;
+            for (; i < board_size; i++)
+            {
+                x -= 1;
+                y += 1;
+                if( x>=0 && y<board_size && field(x,y)==0){
+                    positions.push_back(Coordinates(x,y));
+                }else{
+                    break;
+                }
+            }
+
+            // NW
+            x = c.x;
+            y = c.y; 
+            i = 0;
+            for (; i < board_size; i++)
+            {
+                x += 1;
+                y -= 1;
+                if( x<board_size && y>=0 && field(x,y)==0){
+                    positions.push_back(Coordinates(x,y));
+                }else{
+                    break;
+                }
+            }
+
+            return positions;
+        }
+        /*
+            returns whether the player of corresponding color has lost the Game i.o.w. if noone of colors Amazons can be moved
+        **/
+        bool has_lost(short color){
+            vector<Coordinates> queens = get_queen_positions(color);
+            for (size_t i = 0; i < queens.size(); i++)
+            {
+                for (size_t j = 0; j < operations.size(); j++)
+                {
+                   queens[i].add(operations[j]);
+                   if (0 <= queens[i].x && queens[i].x < board_size && 0 <= queens[i].y && queens[i].y < board_size){
+                        if(field(queens[i].x,queens[i].y)==0){
+                            return false;
+                        }
+                   }
+                   queens[i].sub(operations[j]);
+                }
+                
+            }
+            return true;
+            
+        }      
+
+    };
