daa184f2546680a97adc8a56bf3992a79514dc31
guen-ynk <64133590+guen-ynk@users.noreply.github.com>
2021-11-06 02:08:18 +0100

    Update board.hpp

--- board.hpp
+++ board.hpp
@@ -16,13 +16,44 @@ class Coordinates{
         x = xx;
         y = yy;
     }
+    void add(Coordinates sec){
+        x+=sec.x;
+        y+=sec.y;
+    }
+    void sub(Coordinates sec){
+        x-=sec.x;
+        y-=sec.y;
+    }
+
 };
+
+class Move{
+    public:
+        Coordinates s;
+        Coordinates d;
+        Coordinates a;
+    // No default constructor Error --> seems like object Arrays are not possible ??
+    // possible fixes: Array of refrences ???
+ 
+    Move(Coordinates s_in, Coordinates d_in, Coordinates a_in){ 
+        s = s_in;
+        d = d_in;
+        a = a_in;
+    }
+   
+
+};
+
 class Board {
     public:
         bool whites_turn;
         short board_size;
         short amazons_count;
-        vector<Coordinates> positions;
+        vector<Coordinates> operations {
+            Coordinates(1,0),Coordinates(-1,0),Coordinates(0,1),Coordinates(0,-1),
+            Coordinates(1,1),Coordinates(1,-1),Coordinates(-1,1),Coordinates(-1,-1)
+        };
+
         Matrix<short,Dynamic,Dynamic> field;
         
         Board(){
@@ -46,8 +77,7 @@ class Board {
         /*
             returns vector of coordinates ( amazons )
         **/
-        vector<Coordinates> get_queen_positions(){
-            short color = whites_turn ? 1 : 2;
+        vector<Coordinates> get_queen_positions(short color){
             short counter = 0;
             vector<Coordinates> positions;
             for (size_t i = 0; i < board_size; i++)
@@ -63,6 +93,7 @@ class Board {
                 }
                 
             }
+            return positions;
             
         }
          /*
@@ -177,7 +208,158 @@ class Board {
 
             return positions;
         }
+        /*
+            returns whether the player of corresponding color has lost the Game i.o.w. if noone of colors Amazons can be moved
+        **/
+        bool has_lost(short color){
+            vector<Coordinates> queens = get_queen_positions(color);
+            for (size_t i = 0; i < queens.size(); i++)
+            {
+                for (size_t j = 0; j < operations.size(); j++)
+                {
+                   queens[i].add(operations[j]);
+                   if (0 <= queens[i].x && queens[i].x < board_size && 0 <= queens[i].y && queens[i].y < board_size){
+                        if(field(queens[i].x,queens[i].y)==0){
+                            return false;
+                        }
+                   }
+                   queens[i].sub(operations[j]);
+                }
+                
+            }
+            return true;
+            
+        }      
 
-    };
+
+    vector<Move> generate_moves(short color){
+        vector<Move> moves;
+        vector<Coordinates> queens = get_queen_positions(color);
+        short sx,sy,dx,dy;
+        for (size_t i = 0; i < queens.size(); i++)
+        {
+            vector<Coordinates> amazon_move = get_amazon_moves(queens[i]);
+            for (size_t j = 0; j < amazon_move.size(); j++)
+            {
+                sx = amazon_move[j].x;
+                sy = amazon_move[j].y;
+                dx = sx;
+                dy = sy;
+                Coordinates d = Coordinates(dx,dy);
+
+                // move
+                field(amazon_move[j].x,amazon_move[j].y) = field(queens[i].x,queens[i].y);
+                field(queens[i].x,queens[i].y) = 0;
+
+                // North
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx--;
+                    if(sx>=0 && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sx = amazon_move[j].x;
+                // South
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx++;
+                    if(sx<board_size && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sx = amazon_move[j].x;
+                // West
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sy--;
+                    if(sy>=0 && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sy = amazon_move[j].y;
+                // East
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sy++;
+                    if(sy<board_size && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sy = amazon_move[j].y;
+
+                // Diagonals
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx--;
+                    sy--;
+                    if(sx>=0 && sy>=0 && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sx = amazon_move[j].x;
+                sy = amazon_move[j].y;
+
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx--;
+                    sy++;
+                    if(sx>=0 && sy<board_size && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sx = amazon_move[j].x;
+                sy = amazon_move[j].y;
+
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx++;
+                    sy--;
+                    if(sy>=0 && sx<board_size && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                sx = amazon_move[j].x;
+                sy = amazon_move[j].y;
+
+                for (size_t k = 1; k < board_size; k++)
+                {
+                    sx++;
+                    sy++;
+                    if(sx<board_size && sy<board_size && field(sx,sy)==0){
+                        moves.push_back(Move(queens[i],d,Coordinates(sx,sy)));
+                    }else{
+                        break;
+                    }
+                }
+                // undo move
+                field(queens[i].x,queens[i].y) = field(amazon_move[j].x,amazon_move[j].y);
+                field(queens[i].x,queens[i].y) = 0;
+
+
+                return moves;
+            }
+            
+
+        }
+        
+
+    }
+};
+
 
  
